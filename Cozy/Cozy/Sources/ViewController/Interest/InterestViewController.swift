3f264ca860f38fd8c335017e52383414cdd2c806
iJoom <ghfdld@naver.com>
2020-07-03 18:13:15 +0900

    add Cozy project

---
+++ Cozy/Cozy/Sources/ViewController/Interest/InterestViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InterestViewController.swift
+//  Cozy
+//
+//  Created by IJ . on 2020/07/03.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import UIKit
+
+class InterestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Cozy/Cozy/Sources/ViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cozy
+//
+//  Created by IJ . on 2020/07/03.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
