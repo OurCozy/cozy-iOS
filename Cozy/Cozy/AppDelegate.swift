4fb0e0e45cc3b33833df378eca24d6bc409567be
Choieunji0114 <cheunji8209@naver.com>
2020-07-04 19:09:34 +0900

    Update navigation bar + color extensions

--- Cozy/Cozy/AppDelegate.swift
+++ Cozy/Cozy/AppDelegate.swift
@@ -15,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // change navigation bar tint color 
+        UITabBar.appearance().tintColor = UIColor.coolGreen
+        
         return true
     }
 
