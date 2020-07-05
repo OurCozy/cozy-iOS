4bd224281bf4ad4ab25dac5223d25399aaecf51c
Choieunji0114 <cheunji8209@naver.com>
2020-07-05 16:00:51 +0900

    Update MapNavController

--- Cozy/Cozy/Sources/ViewController/Map/MapViewController.swift
+++ Cozy/Cozy/Sources/ViewController/Map/MapViewController.swift
@@ -8,6 +8,25 @@
 
 import UIKit
 
+// image const
+private struct Const {
+    /// Image height/width for Large NavBar state
+    static let ImageSizeForLargeState: CGFloat = 40
+    /// Margin from right anchor of safe area to right anchor of Image
+    static let ImageRightMargin: CGFloat = 16
+    /// Margin from bottom anchor of NavBar to bottom anchor of Image for Large NavBar state
+    static let ImageBottomMarginForLargeState: CGFloat = 12
+    /// Margin from bottom anchor of NavBar to bottom anchor of Image for Small NavBar state
+    static let ImageBottomMarginForSmallState: CGFloat = 6
+    /// Image height/width for Small NavBar state
+    static let ImageSizeForSmallState: CGFloat = 32
+    /// Height of NavBar for Small state. Usually it's just 44
+    static let NavBarHeightSmallState: CGFloat = 44
+    /// Height of NavBar for Large state. Usually it's just 96.5 but if you have a custom font for the title, please make sure to edit this value since it changes the height for Large state of NavBar
+    static let NavBarHeightLargeState: CGFloat = 96.5
+}
+
+
 class MapViewController: UIViewController {
     
     private let searchImage = UIImageView(image: UIImage(named: "icSearch")) // search image
@@ -22,13 +41,22 @@ class MapViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         
-        self.navigationController?.navigationBar.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: 96)
-        
-        //        self.navigationController?.navigationBar.frame = CGRect(x:0, y:0, width: self.view.frame.size.width, height:96.0)
-        
-        
         if #available(iOS 11.0, *){
             self.navigationController?.navigationBar.prefersLargeTitles = true
+            self.navigationController?.navigationBar.addSubview(searchImage)
+            
+            searchImage.layer.cornerRadius = Const.ImageSizeForLargeState / 2
+            searchImage.clipsToBounds = true
+            searchImage.translatesAutoresizingMaskIntoConstraints = false
+            
+            NSLayoutConstraint.activate([
+                searchImage.rightAnchor.constraint(equalTo: (self.navigationController?.navigationBar.rightAnchor)!, constant: -Const.ImageRightMargin),
+                searchImage.bottomAnchor.constraint(equalTo: (self.navigationController?.navigationBar.bottomAnchor)!, constant: -Const.ImageBottomMarginForLargeState),
+                searchImage.heightAnchor.constraint(equalToConstant: Const.ImageSizeForLargeState),
+                searchImage.widthAnchor.constraint(equalTo: searchImage.heightAnchor)
+            ])
+            
+            
             //            self.navigationController?.navigationBar.backgroundColor
             //            let navBarHeight = UIApplication.shared.statusBarFrame.size.height +
             //            (navigationController?.navigationBar.frame.height ?? 0.0)
