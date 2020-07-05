0e656860c4185618b0f00c9f403ae96170ce1457
Choieunji0114 <cheunji8209@naver.com>
2020-07-05 15:35:32 +0900

    Update map tableview with section

---
+++ Cozy/Cozy/Sources/ViewController/Map/LibraryTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  LibraryTableViewCell.swift
+//  Cozy
+//
+//  Created by 최은지 on 2020/07/05.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import UIKit
+
+class LibraryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var wholeView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+
+}
--- Cozy/Cozy/Sources/ViewController/Map/MapViewController.swift
+++ Cozy/Cozy/Sources/ViewController/Map/MapViewController.swift
@@ -9,22 +9,88 @@
 import UIKit
 
 class MapViewController: UIViewController {
-
+    
+    private let searchImage = UIImageView(image: UIImage(named: "icSearch")) // search image
+    
+    @IBOutlet weak var tableView: UITableView! // table view
+    
+    var sampleItems: [String] = ["hello1", "hello2", "hello3"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        self.navigationController?.navigationBar.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: 96)
+        
+        //        self.navigationController?.navigationBar.frame = CGRect(x:0, y:0, width: self.view.frame.size.width, height:96.0)
+        
+        
+        if #available(iOS 11.0, *){
+            self.navigationController?.navigationBar.prefersLargeTitles = true
+            //            self.navigationController?.navigationBar.backgroundColor
+            //            let navBarHeight = UIApplication.shared.statusBarFrame.size.height +
+            //            (navigationController?.navigationBar.frame.height ?? 0.0)
+            //            print(navBarHeight)
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        if let index = tableView.indexPathForSelectedRow {
+            tableView.deselectRow(at: index, animated: true)
+        }
     }
-    */
+    
+}
 
+extension MapViewController: UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.section {
+        case 0:
+            return 86
+        case 1:
+            return 287
+        default:
+            return 0
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if 0 == section {
+            return 1
+        } else {
+            return sampleItems.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell1") as! CountTableViewCell
+            cell.countLabel.text = "마포구의 독립 서점 87곳"
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell2") as! LibraryTableViewCell
+            cell.wholeView.setViewShadow()
+            
+            return cell
+        }
+    }
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if tableView.contentOffset.y == 0 {
+            // 가장 상단에 있을 때
+            // button.isHidden = false
+        }
+        else {
+            // 가장 상단이 아닐 때
+        }
+    }
 }
