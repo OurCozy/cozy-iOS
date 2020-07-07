7ea5c602372edcaf8eef70813d32262233dee2ca
yangg0228 <yangg0228@gmail.com>
2020-07-07 17:15:03 +0900

    mypage autolayout

--- Cozy/Cozy/Sources/Extension/UIColor+Extension.swift
+++ Cozy/Cozy/Sources/Extension/UIColor+Extension.swift
@@ -22,5 +22,10 @@ extension UIColor {
     @nonobjc class var brownishGrey: UIColor {
       return UIColor(white: 112.0 / 255.0, alpha: 1.0)
     }
+    
+    @nonobjc class var brownGrey: UIColor {
+      return UIColor(white: 163.0 / 255.0, alpha: 1.0)
+    }
+
 
 }
--- Cozy/Cozy/Sources/ViewController/MyPage/MyPageViewController.swift
+++ Cozy/Cozy/Sources/ViewController/MyPage/MyPageViewController.swift
@@ -10,13 +10,32 @@ import UIKit
 
 class MyPageViewController: UIViewController {
 
-
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var lastBookLabel: UILabel!
+    
+    @IBOutlet weak var profileView: UIView!
+    @IBOutlet var profileTapRecognizer: UITapGestureRecognizer!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+//        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(MyPage.goPage(_:)))
+//        self.profileView.addGestureRecognizer(tapGesture)
     }
     
+//    func goPage(sender:UIGestureRecognizer)
+//    {
+//        let storyboard = UIStoryboard(name: MyPage, bundle: nil)
+//
+//        let viewController = storyboard.instantiateViewControllerWithIdentifier("이동할 뷰")
+//
+//                self.navigationController!.pushViewController(viewController, animated: true)
+//    }
 
 
 }
