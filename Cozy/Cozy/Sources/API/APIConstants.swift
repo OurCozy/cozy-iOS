bd2088c501c44eb419719a943ab3ac3d66dfd609
jaeukyang <didwodnr123@gmail.com>
2020-07-09 21:24:22 +0900

    Create: APIConstants, NetworkResult

---
+++ Cozy/Cozy/Sources/API/APIConstants.swift
@@ -0,0 +1,17 @@
+//
+//  APIConstants.swift
+//  Cozy
+//
+//  Created by 양재욱 on 2020/07/09.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import Foundation
+
+struct APIConstants{
+    static let baseURL = "http://13.209.107.188:3000"
+    static let signupURL = APIConstants.baseURL + "/user/signup"
+    static let signinURL = APIConstants.baseURL + "/user/signin"
+    static let mainRecommendationURL = APIConstants.baseURL + "/main/recommendation"
+    static let interestURL = APIConstants.baseURL + "/main/interest"
+}
