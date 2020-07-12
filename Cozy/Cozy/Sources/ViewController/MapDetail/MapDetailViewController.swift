9ee9b9fac6f0d7a472caeceb4eec9b30acdedf9e
Choieunji0114 <cheunji8209@naver.com>
2020-07-12 23:00:20 +0900

    Update MapDetailLabel

--- Cozy/Cozy/Sources/ViewController/MapDetail/MapDetailViewController.swift
+++ Cozy/Cozy/Sources/ViewController/MapDetail/MapDetailViewController.swift
@@ -31,6 +31,8 @@ class MapDetailViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var reviewImageView1: UIImageView!
     @IBOutlet weak var reviewImageView2: UIImageView!
     
+    @IBOutlet weak var moreButton: UIButton!
+    
     let eunpyeongLoc = CLLocationCoordinate2D(latitude: 37.6176125, longitude: 126.9227004) // 은평구 (샘플 서점 위치)
     
     override func viewDidLoad() {
@@ -51,19 +53,35 @@ class MapDetailViewController: UIViewController, UIScrollViewDelegate {
         scrollView.delegate = self
         
         self.setReviewLabel()
+        self.moreButton.settagButton()
     }
     
     func setReviewLabel(){
+        
         self.reviewLabel1.numberOfLines = 0
-        self.reviewLabel1.text = "안도북스는 제가 자주 가는 서점입니다. 안도북스의 분위기를 좋아하고, 책방 속의 작은 소품들과 따뜻한 조명이 퇴근후의 저를 위로하는 것 같아서 자주 방문하는 곳이에요. 퇴근하고 바로 달려가기 조금 빠듯한 시간이지만 자주 찾는 책방입니다. "
+        self.reviewLabel2.numberOfLines = 0
+        
+        let style = NSMutableParagraphStyle()
+        style.lineSpacing = 3.0
+        
+        let reviewText = NSAttributedString(string: "안도북스는 제가 자주 가는 서점입니다. 안도북스의 분위기를 좋아하고, 책방 속의 작은 소품들과 따뜻한 조명이 퇴근후의 저를 위로하는 것 같아서 자주 방문하는 곳이에요. 퇴근하고 바로 달려가기 조금 빠듯한 시간이지만 자주 찾는 책방입니다. ", attributes: [.font : UIFont.systemFont(ofSize: 12)])
+        
+        let attrString = NSMutableAttributedString()
+        attrString.append(reviewText)
+        
+        attrString.addAttributes([.paragraphStyle : style], range: NSMakeRange(0, attrString.length))
+        
+        self.reviewLabel1.attributedText = attrString
+        self.reviewLabel2.attributedText = attrString
         
         self.reviewImageView1.layer.cornerRadius = 5
-        
-        self.reviewLabel2.numberOfLines = 0
-        self.reviewLabel2.text = "안도북스는 제가 자주 가는 서점입니다. 안도북스의 분위기를 좋아하고, 책방 속의 작은 소품들과 따뜻한 조명이 퇴근후의 저를 위로하는 것 같아서 자주 방문하는 곳이에요. 퇴근하고 바로 달려가기 조금 빠듯한 시간이지만 자주 찾는 책방입니다."
         self.reviewImageView2.layer.cornerRadius = 5
     }
     
+    // 더보기 버튼 클릭
+    @IBAction func clickDetailButton(_ sender: UIButton) {
+        
+    }
     
     @IBAction func goReview(_ sender: UIButton) {
         
@@ -125,11 +143,18 @@ class MapDetailViewController: UIViewController, UIScrollViewDelegate {
     
     func setExplainLabel(){
         self.explainLabel.numberOfLines = 0
-        self.explainLabel.text = """
-        스토리지북앤필름 너무 좋은 독립서점이에요. 해방촌의 분위기를
-        좋아하는데 이 책방이 한 몫하는것 같아요. 골목길에 있지만 일부러
-        찾아오는 책방입니다! 대형서점에 없는 책들도 많고 가게 자체도
-        """
+        
+        let style = NSMutableParagraphStyle()
+        style.lineSpacing = 3.0
+        
+        let reviewText = NSAttributedString(string: "스토리지북앤필름 너무 좋은 독립서점이에요. 해방촌의 분위기를 좋아하는데 이 책방이 한 몫하는것 같아요. 골목길에 있지만 일부러 찾아오는 책방입니다! 대형서점에 없는 책들도 많고 가게 자체도", attributes: [.font : UIFont.systemFont(ofSize: 12)])
+        
+        let attrString = NSMutableAttributedString()
+        attrString.append(reviewText)
+        
+        attrString.addAttributes([.paragraphStyle : style], range: NSMakeRange(0, attrString.length))
+        
+        self.explainLabel.attributedText = attrString
     }
     
     @IBAction func clickBookMarkButton(_ sender: UIButton) {
