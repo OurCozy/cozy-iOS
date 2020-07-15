1934ede45bfb381fc598675e95d788bc54785478
jaeukyang <didwodnr123@gmail.com>
2020-07-15 21:41:15 +0900

    Create: EmailLoginView

--- Cozy/Cozy/Sources/ViewController/Login/EmailLoginViewController.swift
+++ Cozy/Cozy/Sources/ViewController/Login/EmailLoginViewController.swift
@@ -9,59 +9,51 @@
 import UIKit
 
 class EmailLoginViewController: UIViewController {
+    
+    let border1 = CALayer()
+    let border2 = CALayer()
 
-    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var idTextField: UITextField!
     @IBOutlet weak var pwTextField: UITextField!
     @IBOutlet weak var autoLoginButton: UIButton!
     @IBOutlet weak var autoLoginLabel: UILabel!
-    @IBOutlet weak var seperateLine: UIView!
-    @IBOutlet weak var checkIdLabel: UILabel!
-    @IBOutlet weak var checkPwLabel: UILabel!
-    
+    @IBOutlet weak var checkIdButton: UIButton!
+    @IBOutlet weak var checkPwButton: UIButton!
+    @IBOutlet weak var simpleLine: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
     }
     
     override func viewDidLayoutSubviews() {
         setTextFieldStyle()
-        setButtonStyle()
-        setLabelStyle()
+        autoLoginButton.layer.cornerRadius = 9
+        autoLoginButton.layer.borderWidth = 1
+        autoLoginButton.layer.borderColor = UIColor.veryLightPinkTwo.cgColor
+        autoLoginLabel.textColor = UIColor.veryLightPinkTwo
+        checkIdButton.tintColor = UIColor.brownishGrey
+        checkPwButton.tintColor = UIColor.brownishGrey
+        simpleLine.backgroundColor = UIColor.brownGrey
     }
 
-
     func setTextFieldStyle(){
-        emailTextField.borderStyle = .none
-        let border1 = CALayer()
-        border1.frame = CGRect(x: 0, y: emailTextField.frame.size.height-1, width: emailTextField.frame.width, height: 1)
+        idTextField.borderStyle = .none
+        
+        border1.frame = CGRect(x: 0, y: idTextField.frame.size.height-1, width: idTextField.frame.width, height: 1)
         border1.backgroundColor = UIColor.veryLightPinkTwo.cgColor
-        emailTextField.layer.addSublayer((border1))
-        emailTextField.textAlignment = .left
-        emailTextField.textColor = UIColor.black
+        idTextField.layer.addSublayer((border1))
+        idTextField.textAlignment = .left
+        idTextField.textColor = UIColor.black
         
         pwTextField.borderStyle = .none
-        let border2 = CALayer()
+        
         border2.frame = CGRect(x: 0, y: pwTextField.frame.size.height-1, width: pwTextField.frame.width, height: 1)
         border2.backgroundColor = UIColor.veryLightPinkTwo.cgColor
         pwTextField.layer.addSublayer((border2))
         pwTextField.textAlignment = .left
         pwTextField.textColor = UIColor.black
-    }
-    
-    func setButtonStyle(){
-        autoLoginButton.layer.borderWidth = 1
-        autoLoginButton.layer.borderColor = UIColor.veryLightPinkTwo.cgColor
-        autoLoginButton.layer.cornerRadius = 9
-    }
-    
-    func setLabelStyle(){
-        autoLoginLabel.textColor = UIColor.veryLightPinkTwo
-        checkIdLabel.textColor = UIColor.brownishGrey
-        checkPwLabel.textColor = UIColor.brownishGrey
-        seperateLine.backgroundColor = UIColor.brownGrey
     }
     /*
     // MARK: - Navigation
